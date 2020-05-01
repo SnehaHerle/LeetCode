f511e59fac9c3e6d0b4a2f6c23b7e3b033ca3381
Sneha Herle <snehaherle@gmail.com>
2020-05-01 23:56:52 +0530

    Update First Bad Version.cpp

--- First Bad Version.cpp
+++ First Bad Version.cpp
@@ -3,6 +3,16 @@
 // The API isBadVersion is defined for you.
 // bool isBadVersion(int version);
 
+/*
+If you are setting mid = (left + right)/ 2 you have to be very careful. 
+Unless you are using a language that does not overflow such as Python, left + right could overflow. 
+One way to fix this is to use left + {right - left}/2  instead.
+
+If you fall into this subtle overflow bug, you are not alone. 
+Even Jon Bentley's own implementation of binary search had this overflow bug and remained undetected for over twenty years.
+*/
+
+
 class Solution {
 public:
     
