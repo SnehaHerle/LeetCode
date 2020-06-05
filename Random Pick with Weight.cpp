a668944dde3bd73d9002dd2fc2426e8b8bc0a484
Sneha Herle <snehaherle@gmail.com>
2020-06-05 23:55:19 +0530

    Create Random Pick with Weight.cpp

---
+++ Random Pick with Weight.cpp
@@ -0,0 +1,24 @@
+//https://leetcode.com/explore/challenge/card/june-leetcoding-challenge/539/week-1-june-1st-june-7th/3351/
+
+//Random Pick with Weight
+
+
+class Solution {
+    vector<int> v;
+public:
+    Solution(vector<int>& w) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
+        
+        v.pb(w[0]);
+        for(int i=1;i<w.size();++i)
+            v.pb(v.back()+w[i]);    //Cumulative SUM
+    }
+    
+    int pickIndex() {
+        int rand_weight = rand()%(v.back()+1);
+        return upper_bound(v.begin(),v.end(),rand_weight)-v.begin();
+        
+    }
+};
