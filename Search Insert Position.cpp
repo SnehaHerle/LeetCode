620ea97bb25d593a3d84c9300929f450a96001f3
Sneha Herle <snehaherle@gmail.com>
2020-06-11 00:03:33 +0530

    Update Search Insert Position.cpp

--- Search Insert Position.cpp
+++ Search Insert Position.cpp
@@ -1,31 +1,52 @@
+//https://leetcode.com/explore/featured/card/june-leetcoding-challenge/540/week-2-june-8th-june-14th/3356/
+
+//Search Insert Position
+
+//O((logN)  - Binary Search
+
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) 
     {
+        int ans = 0, flag = 0, beg = 0, end = nums.size()-1 , mid = 0;
         if(target < nums[0])
-            return 0;
+        {
+            cout<<target<<" "<<nums[0];
+            ans = 0;
+        }
+        else if(target > nums[end])
+            ans = end+1;
+            
         
         else
         {
-            int beg = 0, end = nums.size()-1 , mid = 0;
-            
             while(beg<=end)
             {
                 mid = (beg + end)/2;
                 if(nums[mid] == target)
-                    return mid;
-                else if(nums[mid] < mid )
+                {
+                    ans = mid;
+                    flag = 1;
+                    break;
+                }
+                else if(nums[mid] < target )
                 {
                     if(nums[mid+1] > target)
-                        return mid+1;
+                    {
+                        ans = mid+1;
+                        flag = 1;
+                        break;
+                    }
                     else
                         beg = mid + 1;
                 }
                 else
                     end = mid - 1;
             }
-            return mid;
+            if(flag == 0)
+                ans = mid;
             
         }
+        return ans;
     }
 };
