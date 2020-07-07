59fbb2f74061b65f2b0269195da27fa8c9a4408e
Sneha Herle <snehaherle@gmail.com>
2020-07-07 23:37:27 +0530

    Create Island Perimeter.cpp

---
+++ Island Perimeter.cpp
@@ -0,0 +1,21 @@
+class Solution 
+{
+public:
+    int islandPerimeter(vector<vector<int>>& grid) 
+    {
+        int perimeter = 0;
+        for (int i = 0; i < grid.size(); i++) 
+        {
+            for (int j = 0; j < grid[0].size(); j++) 
+            {
+                if (grid[i][j] == 1) {
+                    perimeter += 4;
+                    if (i < grid.size() - 1) perimeter -= 2 * grid[i + 1][j];
+                    if (j < grid[0].size() - 1) perimeter -= 2 * grid[i][j + 1];
+                }
+            }
+        }
+
+        return perimeter;
+    }
+};
