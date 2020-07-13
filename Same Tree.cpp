e562079ad758b8a67167be9952ee177f7bec935c
Sneha Herle <snehaherle@gmail.com>
2020-07-14 00:24:08 +0530

    Update Same Tree.cpp

--- Same Tree.cpp
+++ Same Tree.cpp
@@ -1,11 +1,19 @@
-let isSameTree = function(p, q) {
-    if (p === null || q === null) {
-        return p === q;
-    }
+//https://leetcode.com/explore/featured/card/july-leetcoding-challenge/545/week-2-july-8th-july-14th/3389/
+
+//Same Tree.
+
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) 
+    {
+        if (p == NULL || q == NULL) {
+        return p == q;
+        }
     
-    if (p.val !== q.val) {
+        if (p->val != q->val) {
         return false;
-    }
+        }
     
-    return isSameTree(p.right, q.right) && isSameTree(p.left, q.left);
+        return isSameTree(p->right, q->right) && isSameTree(p->left, q->left);   
+    }
 };
