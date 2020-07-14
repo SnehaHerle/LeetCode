c84d5450a108e0d3dcc7f09be155e3975d831ff1
Sneha Herle <snehaherle@gmail.com>
2020-07-15 00:54:52 +0530

    Update Angle.cpp

--- Angle.cpp
+++ Angle.cpp
@@ -1,1 +1,15 @@
-Test
+class Solution {
+public:
+    double angleClock(int hour, int minutes) 
+    {
+        if(hour == 12)
+            hour = 0;
+        double diff = (minutes*6)-((hour*60*0.5) + (minutes*0.5));
+        if(diff < 0)
+            diff = -1*diff;
+        cout<<diff;
+        if(diff > 180)
+            return 360 - diff;
+        return diff;
+    }
+};
