92c8c7180bb5adc4960cff56cb5f6f09edfc719b
Sneha Herle <snehaherle@gmail.com>
2022-01-17 07:00:26 +0530

    Create Word Pattern file

---
+++ Word Pattern.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool wordPattern(std::string pa, std::string str) 
+{
+    std::string w;                 
+    std::stringstream ss(str);
+    std::vector<std::string> words; 
+
+    while (ss >> w)
+    {
+        words.push_back(w);
+    }
+    
+    if(words.size() != pa.size()) return false;
+    
+    std::map<char, std::string> ma;
+    std::map<std::string, char> mb;
+    
+    for(size_t i = 0; i < words.size(); i++)
+    {
+        ma[pa[i]] = words[i];
+        mb[words[i]] = pa[i];
+    }
+    
+    for(size_t i = 0; i < words.size(); i++)
+    {
+        if(ma[pa[i]] != words[i] || mb[words[i]] != pa[i])
+        {
+            return false;
+        }
+    }
+     
+    return true;
+}
+};
