edf333d0e93ef43b4d54f01db9de9bf1acb73d4e
Sneha Herle <snehaherle@gmail.com>
2022-01-19 06:53:16 +0530

    Create Count Items Matching a Rule file

---
+++ Count Items Matching a Rule.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int countMatches(vector<vector<string>>& items, string ruleKey, string ruleValue) {
+        int countMatchingItems = 0;
+        for(int i = 0; i < items.size(); i++){
+            if(ruleKey == "type"){
+                if(items[i][0] == ruleValue)
+                    countMatchingItems++;
+            } else if(ruleKey == "color"){
+                if(items[i][1] == ruleValue)
+                    countMatchingItems++;
+            } else if(ruleKey == "name"){
+                if(items[i][2] == ruleValue)
+                    countMatchingItems++;
+            }
+        }
+        return countMatchingItems;
+    }
+};
